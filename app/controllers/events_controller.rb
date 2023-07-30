ff2be747e9a4d019d0bcedf96525e75f10dde9ad
Christopher Kinoshita <happychristopher777@gmail.com>
2023-07-29 23:16:10 -0700

    Added new show create actions in controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,24 @@ class EventsController < ApplicationController
     # indexes all events
     @events = Event.all
   end
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save 
+      redirect_to events_path
+    else
+      render 'new'
+    end
+  end
+  def new
+    @event = current_user.events.build
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+  private 
+  
+  def event_params 
+    params.require(:event).permit(:name, :location, :date)
+  end
 end
