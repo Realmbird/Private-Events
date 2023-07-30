16f5a154fc435648c57a908c3d97cec5996e63b1
Christopher Kinoshita <happychristopher777@gmail.com>
2023-07-29 22:23:55 -0700

    created Creator_id

---
+++ db/migrate/20230730051204_add_creator_reference_events.rb
@@ -0,0 +1,5 @@
+class AddCreatorReferenceEvents < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :events, :creator
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_30_045856) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_30_051204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_30_045856) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
