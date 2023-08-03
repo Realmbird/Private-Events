43effb31cc304544df8af1add9f8e34c64de82e7
Christopher Kinoshita <happychristopher777@gmail.com>
2023-08-03 09:51:35 -0700

    Added Scopes to models

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,6 @@ class Event < ApplicationRecord
   
   has_many :invitations, foreign_key: "attended_event_id"
   has_many :attendees, through: :invitations, source: :attendee
+  scope :past, -> {where("date < :current_date", {current_date: Time.now})}
+  scope :future, -> {where("date > :current_date", {current_date: Time.now})}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   has_many :events, foreign_key: "creator_id", dependent: :destroy 
   # belongs_to :attended_event, class_name: "Event", foreign_key: "attendee_id"
   has_many :invitations, foreign_key: "attendee_id"
-  has_many :attended_events, through: :invitations, source: :attended_event  
+  has_many :attended_events, through: :invitations, source: :attended_event
+  
 end
