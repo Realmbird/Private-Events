8d64b652bcda0dc66f86987a24a385de3637bf1c
Christopher Kinoshita <happychristopher777@gmail.com>
2023-08-03 10:50:25 -0700

    Added ability for users to edit or delete events, Added Navbar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,6 +12,23 @@ class EventsController < ApplicationController
       render 'new'
     end
   end
+  def edit 
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to root_path
+  end
   def new
     @event = current_user.events.build
   end
